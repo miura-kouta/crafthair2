4cccef57476e5011e413f5f54d4803cc0418d501
miura-kouta <mk.craft.website@gmail.com>
2025-02-11 17:43:18 +0900

    topページ修正（画像の大きさや配置、くずれなど）

--- js/main.js
+++ js/main.js
@@ -32,20 +32,22 @@ navi.addEventListener("click", function () {
 });
 
 // モーダルウインドウ
-
 document.addEventListener("DOMContentLoaded", function () {
-    const modal = document.querySelector(".modal__container"); 
-    const img = modal.querySelector("img");
+    const modal = document.querySelector(".modal__container");
 
-    document.querySelectorAll(".attachment-medium_large").forEach(function (popupImg) {
-        popupImg.addEventListener("click", function (event) {
-            event.preventDefault(); 
-            img.src = this.src;
-            modal.style.display = "block";
+    if (modal) {
+        const img = modal.querySelector("img");
+
+        document.querySelectorAll(".attachment-medium_large").forEach(function (popupImg) {
+            popupImg.addEventListener("click", function (event) {
+                event.preventDefault();
+                img.src = this.src;
+                modal.style.display = "block";
+            });
         });
-    });
 
-    modal.addEventListener("click", function () {
-        this.style.display = "none";
-    });
+        modal.addEventListener("click", function () {
+            this.style.display = "none";
+        });
+    }
 });
